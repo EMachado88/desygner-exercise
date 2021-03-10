7d715631cfe66db8630d30acd40d953a6009ff8e
Emanuel Machado <emanuel@emachado.pt>
2021-03-10 11:24:17 +0000

    Added tests for TempTracker

---
+++ TempTracker.test.js
@@ -0,0 +1,20 @@
+const TempTracker = require('./TempTracker')
+
+const tempTracker = new TempTracker
+
+
+test('adds 30, 10 and 20, min should be 10', () => {
+  tempTracker.setTemp(30)
+  tempTracker.setTemp(10)
+  tempTracker.setTemp(20)
+
+  expect(tempTracker.getMinTemp()).toBe(10)
+});
+
+test('max should be 30', () => {
+  expect(tempTracker.getMaxTemp()).toBe(30)
+});
+
+test('avg should be 20', () => {
+  expect(tempTracker.getAvgTemp()).toBe(20)
+});
